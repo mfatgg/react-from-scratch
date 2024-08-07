0dfa706c3fd19682a470b866c685c2177d7717d7
mfatgg <mfatgg00@gmail.com>
2024-08-07 17:23:45 +0200

    simple react boilerplate for js with lint, test, tailwindcss and webpack support

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './app.css';
+
+function App() {
+	return (
+		<h1 className="text-primary text-4xl font-bold">
+			Hello world! I am using React
+		</h1>
+	);
+}
+
+export default App;
