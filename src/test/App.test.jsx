7e3950da97e2e92bf8c9c26be57393863a00b75c
mfatgg <mfatgg00@gmail.com>
2024-08-08 18:27:54 +0200

    format all files with prettier

--- src/test/App.test.jsx
+++ src/test/App.test.jsx
@@ -1,11 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import React from 'react';
-import App from '../App.jsx';
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import App from "../App.jsx";
 
-describe('App tests', () => {
-	it('should contains the heading 1', () => {
-		render(<App />);
-		const heading = screen.getByText(/Hello world! I am using React/i);
-		expect(heading).toBeInTheDocument();
-	});
+describe("App tests", () => {
+  it("should contains the heading 1", () => {
+    render(<App />);
+    const heading = screen.getByText(/Hello world! I am using React/i);
+    expect(heading).toBeInTheDocument();
+  });
 });
