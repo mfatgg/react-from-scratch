0599f55dc38e43eeb898068ed9a87cde7e30ec42
mfatgg <mfatgg00@gmail.com>
2024-08-08 18:24:59 +0200

    improve linting and format rules + add standard node packages for real world project

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,15 @@
 module.exports = {
-	collectCoverage: true,
-	collectCoverageFrom: ['src/**/*.{js,jsx}'],
-	coverageDirectory: 'coverage',
-	moduleNameMapper: {
-		'\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-			'<rootDir>/__mocks__/fileMock.js',
-		'\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-	},
-	testEnvironment: 'jsdom',
-	setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/*.{js,jsx}"],
+  coverageDirectory: "coverage",
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/__mocks__/fileMock.js",
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+  testEnvironment: "jest-fixed-jsdom",
+  testEnvironmentOptions: {
+    customExportConditions: [""],
+  },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
 };
