d3f863854d06c052e69cd5ca9c86cb83f8f5fd3a
mfatgg <mfatgg00@gmail.com>
2024-08-08 18:37:10 +0200

    split webpack config into base, development and production parts + auto-select config based on webpack environment

---
+++ webpack.base.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./index.jsx",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+  },
+  target: "web",
+  resolve: {
+    extensions: [".js", ".jsx", ".json"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+    }),
+  ],
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,10 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
+const { merge } = require("webpack-merge");
 
-module.exports = {
-  entry: "./index.jsx",
-  mode: "development",
-  output: {
-    path: path.resolve(__dirname, "./dist"),
-    filename: "index_bundle.js",
-  },
-  target: "web",
-  devServer: {
-    port: "5000",
-    static: {
-      directory: path.join(__dirname, "public"),
-    },
-    open: true,
-    hot: true,
-    liveReload: true,
-  },
-  resolve: {
-    extensions: [".js", ".jsx", ".json"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: "babel-loader",
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader", "postcss-loader"],
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, "public", "index.html"),
-    }),
-  ],
+const baseConfig = require("./webpack.base");
+
+module.exports = (env) => {
+  const config = require(
+    `./webpack.${env.production ? "production" : "development"}`
+  );
+  return merge(baseConfig, config);
 };
